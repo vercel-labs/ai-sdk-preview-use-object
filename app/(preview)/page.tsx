d1afd216244a03fefa2bf514b7da935beff2a542
jeremyphilemon <git@jrmyphlmn.com>
2024-08-02 22:51:14 +0530

    Add deploy button

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -3,7 +3,7 @@
 
 "use client";
 
-import { ObjectIcon, VercelIcon } from "@/components/icons";
+import { GitIcon, ObjectIcon, VercelIcon } from "@/components/icons";
 import { experimental_useObject } from "ai/react";
 import { useRef, useState } from "react";
 import { motion } from "framer-motion";
@@ -114,7 +114,7 @@ export default function Home() {
           <motion.div className="h-[350px] px-4 w-full md:w-[500px] md:px-0 pt-20">
             <div className="border rounded-lg p-6 flex flex-col gap-4 text-zinc-500 text-sm dark:text-zinc-400 dark:border-zinc-700">
               <p className="flex flex-row justify-center gap-4 items-center text-zinc-900 dark:text-zinc-50">
-                <VercelIcon />
+                <VercelIcon size={16} />
                 <span>+</span>
                 <ObjectIcon />
               </p>
@@ -138,6 +138,30 @@ export default function Home() {
           </motion.div>
         )}
       </div>
+
+      <motion.div
+        className="flex flex-row gap-4 items-center justify-between fixed bottom-6 text-xs "
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <Link
+          target="_blank"
+          href="https://github.com/vercel-labs/ai-sdk-preview-use-object"
+          className="flex flex-row gap-2 items-center border px-2 py-1.5 rounded-md hover:bg-zinc-100 dark:border-zinc-700 dark:hover:bg-zinc-800"
+        >
+          <GitIcon />
+          View Source Code
+        </Link>
+
+        <Link
+          target="_blank"
+          href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel-labs%2Fai-sdk-preview-use-object&env=OPENAI_API_KEY&envDescription=API%20keys%20needed%20for%20this%20application.&envLink=platform.openai.com"
+          className="flex flex-row gap-2 items-center bg-zinc-900 px-2 py-1.5 rounded-md text-zinc-50 hover:bg-zinc-950 dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-50"
+        >
+          <VercelIcon size={14} />
+          Deploy with Vercel
+        </Link>
+      </motion.div>
     </div>
   );
 }
